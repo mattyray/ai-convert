fe7f19fa0ec74f2c5afd5633bb991bed509bf9b5
mattyray <mnraynor90@gmail.com>
2025-04-02 19:46:14 -0400

    Added custom password change and password change done templates; updated URL configurations for password change views

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
+
 from .views import SignupPageView, DashboardView, custom_logout, ProfileEditView
 
 urlpatterns = [
@@ -7,4 +9,14 @@ urlpatterns = [
     path('dashboard/', DashboardView.as_view(), name='dashboard'),
     path('profile/edit/', ProfileEditView.as_view(), name='profile_edit'),
     path('logout/', custom_logout, name='logout'),
+        path(
+        'password/change/',
+        auth_views.PasswordChangeView.as_view(template_name='account/password_change.html'),
+        name='password_change'
+    ),
+    path(
+        'password/change/done/',
+        auth_views.PasswordChangeDoneView.as_view(template_name='account/password_change_done.html'),
+        name='password_change_done'
+    ),
 ]

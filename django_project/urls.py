9b7cc12e602a498e220c5e30987813b9e60db9ff
mattyray <mnraynor90@gmail.com>
2025-04-05 13:27:43 -0400

    blog - migrations

--- django_project/urls.py
+++ django_project/urls.py
@@ -6,15 +6,11 @@ from django.conf import settings
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),         # Custom signup and logout routes
-    path("accounts/", include("allauth.urls")),  # Django-allauth handles login, signup, etc.
-    path("", include("pages.urls")),           
-    path('store/', include('store.urls')),
-
-
+    path("accounts/", include("allauth.urls")),            # Django-allauth handles login, signup, etc.
+    path("blog/", include("blog.urls")),                   # Blog URLs
+    path("", include("pages.urls")),                       
+    path("store/", include("store.urls")),
 ]
-
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-

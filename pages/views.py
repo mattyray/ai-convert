4c557647be13f3c41053038e76b31215b4ce3e6e
mattyray <mnraynor90@gmail.com>
2025-04-06 13:42:47 -0400

    Added home page views

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,11 @@
-# from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
+from blog.models import Post  # 👈 Import the Post model
 
-# class HomePageView(LoginRequiredMixin, TemplateView):
-class HomePageView(TemplateView):  # 👈 No login required
+class HomePageView(TemplateView):
     template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Add 3 most recently updated published posts
+        context["recent_posts"] = Post.objects.filter(is_published=True).order_by('-updated_date')[:3]
+        return context

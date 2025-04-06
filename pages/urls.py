54a5f6fa30a2f20bd38088fa28023d6c6c3d5ede
mattyray <mnraynor90@gmail.com>
2025-04-06 16:36:30 -0400

    press buildout - pages - urls

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, PressPageView
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),  # Ensure this exists!
+    path("press/", PressPageView.as_view(), name="press"),
+    
+    
     
 ]

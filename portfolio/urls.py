acd8e6a12dd730490c6e2ae558a1151109812923
mattyray <mnraynor90@gmail.com>
2025-04-07 18:34:42 -0400

    port views

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import PortfolioView
+from .views import PortfolioView, ProjectDetailView
 
 urlpatterns = [
     path("", PortfolioView.as_view(), name="portfolio"),
+    path("<slug:slug>/", ProjectDetailView.as_view(), name="project_detail"),
 ]

cbf0cf67a70698dd6b5a219fec0955b71deeedad
mattyray <mnraynor90@gmail.com>
2025-04-10 11:21:55 -0400

    contact - captcha - pages -urls

--- pages/urls.py
+++ pages/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import HomePageView, PressPageView
+from .views import HomePageView, PressPageView, contact_view
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),  # Ensure this exists!
     path("press/", PressPageView.as_view(), name="press"),
+    path("contact/", contact_view, name="contact"),
     
     
     

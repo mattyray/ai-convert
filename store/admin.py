50669dd26846979fb72c3dc4ce39d3d95be529a8
mattyray <mnraynor90@gmail.com>
2025-04-12 19:06:24 -0400

    added stuff to admin

--- store/admin.py
+++ store/admin.py
@@ -1,13 +1,27 @@
 from django.contrib import admin
-from .models import Product, Order, OrderItem, Review
+from .models import Product, Collection, Order, OrderItem, Review
 
+# --------------------
+# PRODUCT ADMIN
+# --------------------
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('title', 'price', 'stock', 'created_at')
+    list_display = ('title', 'product_type', 'price', 'stock', 'created_at')
     prepopulated_fields = {'slug': ('title',)}
     search_fields = ('title', 'description')
     list_filter = ('created_at',)
 
+# --------------------
+# COLLECTION ADMIN
+# --------------------
+@admin.register(Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at')
+    prepopulated_fields = {'slug': ('name',)}
+
+# --------------------
+# ORDER ADMIN
+# --------------------
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
     extra = 0
@@ -18,6 +32,9 @@ class OrderAdmin(admin.ModelAdmin):
     list_filter = ('status', 'created_at')
     inlines = [OrderItemInline]
 
+# --------------------
+# REVIEW ADMIN
+# --------------------
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
     list_display = ('product', 'user', 'rating', 'created_at')

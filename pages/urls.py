563c8adf0811008ef0328271135cf5d55a9e8653
mattyray <mnraynor90@gmail.com>
2025-05-06 15:56:24 -0400

    fixed namespcing for pages app

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import HomePageView, PressPageView, contact_view
 from . import views
+
+app_name = "pages"
+
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),  # Ensure this exists!
     path("press/", PressPageView.as_view(), name="press"),

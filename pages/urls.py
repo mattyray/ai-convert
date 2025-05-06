5887a844e1df4d5cbd4f8eacafd5d08c633b290b
mattyray <mnraynor90@gmail.com>
2025-05-06 15:39:09 -0400

    pages/views

--- pages/urls.py
+++ pages/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import HomePageView, PressPageView, contact_view
-
+from . import views
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),  # Ensure this exists!
     path("press/", PressPageView.as_view(), name="press"),
     path("contact/", contact_view, name="contact"),
+    path('story/', views.StoryPageView.as_view(), name='story'),
+
     
     
     

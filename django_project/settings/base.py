283413da98f96c5a3a3ad11e5206b7b9c45119cd
mattyray <mnraynor90@gmail.com>
2025-05-11 17:31:07 -0400

    🔧 Fix template DIRS path for CI

--- django_project/settings/base.py
+++ django_project/settings/base.py
@@ -89,7 +89,7 @@ ROOT_URLCONF = 'django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR.parent / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

7ec88c4ec0be48010dce074a0764a7112ebe480e
mattyray <mnraynor90@gmail.com>
2025-05-13 15:16:27 -0400

    Fix STATIC_ROOT and STATICFILES_DIRS path for collectstatic

--- django_project/settings/base.py
+++ django_project/settings/base.py
@@ -158,8 +158,8 @@ USE_TZ = True
 
 # Static files
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [BASE_DIR.parent / "static"]
+STATIC_ROOT = BASE_DIR.parent / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Media (Cloudinary)

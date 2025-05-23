d6f42e49615fb2fb7c7cd02e1b18148437cb9670
mattyray <mnraynor90@gmail.com>
2025-05-23 12:45:17 -0400

    pypi chatwidget - changed onboarding.json to teach ppl how to uyse it, also added info to homepage

--- django-chatwidget/chatwidget/urls.py
+++ django-chatwidget/chatwidget/urls.py
@@ -1,10 +1,13 @@
 # chatwidget/urls.py
 
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
+
 from .views import ChatAPIView
 
 app_name = "chatwidget"
 
 urlpatterns = [
-    path("api/", ChatAPIView.as_view(), name="api"),
+    path("api/", csrf_exempt(ChatAPIView.as_view()), name="api"),  # ✅ CSRF exempted
 ]
+ 

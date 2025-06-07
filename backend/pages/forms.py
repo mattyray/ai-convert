6d595c0ff7301baa0253f1b0994ba924b7d8d611
mattyray <mnraynor90@gmail.com>
2025-06-07 19:54:37 -0400

    gutting old structure

--- backend/pages/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from django_recaptcha.fields import ReCaptchaField
-from django_recaptcha.widgets import ReCaptchaV2Checkbox  # ✅ required!
-
-class ContactForm(forms.Form):
-    name = forms.CharField(max_length=100)
-    email = forms.EmailField()
-    message = forms.CharField(widget=forms.Textarea)
-    captcha = ReCaptchaField(widget=ReCaptchaV2Checkbox)

5e1b00ad58b807d7f1101d8bcf6ac3e9c416a81d
mattyray <mnraynor90@gmail.com>
2025-06-07 19:54:37 -0400

    gutting old structure

--- backend/blog/tests/test_forms.py
+++
@@ -1,6 +0,0 @@
-from django.test import TestCase
-
-class BlogModelsSmokeTest(TestCase):
-    def test_smoke(self):
-        # trivial test to verify blog app loads
-        self.assertTrue(True)
--- backend/store/tests/test_models.py
+++
@@ -1,6 +0,0 @@
-from django.test import TestCase
-
-class StoreModelsSmokeTest(TestCase):
-    def test_smoke(self):
-        # trivial test to verify store app loads
-        self.assertTrue(True)

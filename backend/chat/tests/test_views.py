f1138526530d87fcd1db5960c34e1a36bc79a7c3
mattyray <mnraynor90@gmail.com>
2025-06-09 17:20:41 -0400

    gut acct/tests

--- backend/chat/tests/test_views.py
+++
@@ -1,9 +0,0 @@
-#chat/tests/test_views.py
-from django.test import TestCase
-from django.urls import reverse
-
-class ChatInterfaceViewTest(TestCase):
-    def test_chat_interface_loads(self):
-        response = self.client.get(reverse("chat:interface"))
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "AI Assistant")

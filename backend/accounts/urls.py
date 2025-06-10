4faa50b9eaa901207718a03d219bfc40e534c609
mattyray <mnraynor90@gmail.com>
2025-06-10 16:41:15 -0400

    ACCT/URL

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from .views import SignupAPIView, UserProfileAPIView
+from .api_auth import CustomAuthToken
+
 
 app_name = "accounts"
 
 urlpatterns = [
     path("signup/", SignupAPIView.as_view(), name="signup"),
     path("me/", UserProfileAPIView.as_view(), name="user-profile"),
+    path("login/", CustomAuthToken.as_view(), name="token-login"),
+
+
 ]

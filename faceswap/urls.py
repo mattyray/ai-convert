2e04b8efc1eaa9d781871ef8038ee60b5e980d18
mattyray <mnraynor90@gmail.com>
2025-06-14 10:27:20 -0400

    creeating faceswap app - urls.py

---
+++ faceswap/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from .views import (
+    FaceSwapCreateView,
+    FaceSwapListView, 
+    FaceSwapDetailView,
+    FaceSwapStatusView
+)
+
+app_name = "faceswap"
+
+urlpatterns = [
+    path("create/", FaceSwapCreateView.as_view(), name="create"),
+    path("jobs/", FaceSwapListView.as_view(), name="list"),
+    path("jobs/<int:pk>/", FaceSwapDetailView.as_view(), name="detail"),
+    path("status/<int:job_id>/", FaceSwapStatusView.as_view(), name="status"),
+]

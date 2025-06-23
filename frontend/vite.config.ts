5298bd3b1407e67f8740aa5f956a139f1eecfd8f
mattyray <mnraynor90@gmail.com>
2025-06-23 17:04:52 -0400

    frontend - vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173,
+    host: true,
+    open: true
+  },
+  css: {
+    postcss: './postcss.config.js'
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true
+  }
 })
